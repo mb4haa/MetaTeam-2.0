35fd286948a7c9437632246586fce1d7a92629fd
Mahmoud Bahaa <mbahaa1997@hotmail.com>
2018-02-17 22:10:23 +0200

    All Done with handling the exception just need to Sort inputs

--- classes/metaTeam/DBApp.java
+++ classes/metaTeam/DBApp.java
@@ -1,43 +1,36 @@
 package metaTeam;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Scanner;
 
 public class DBApp {
 
 	private static String[] acceptedTypes = {"java.lang.Integer","java.lang.String", "java.lang.Double", "java.lang.Boolean", "java.util.Date"};
 
 	public static void init() {
-		File file = new File("MetaData.csv");
 	}
 
 	public static void createTable(String tableName, String clusteringKey, Hashtable<String,String> colNameType) throws DBAppException, IOException {
 		FileWriter fWriter = new FileWriter("./MetaData.csv" , true);
 		BufferedWriter bw = new BufferedWriter(fWriter);
-//		try {
-//		for(Enumeration<String> e = colNameType.keys();e.hasMoreElements();) {
-//			String colName = e.nextElement();
-//			String colType = colNameType.get(colName);
-//			for (String type : acceptedTypes) {
-//				if(colType.equals(type)) {
-//					break;
-//				}
-//				else if(type.equals("Java.util.Date")) {
-//					throw (DBAppException d);
-//				}
-//			}
-//		}
-//		}
-//		catch(DBAppException) {
-//			System.out.println("Wrong type");
-//		}
 		String toBeAdded = "";
+		for(Enumeration<String> e = getKeys(colNameType);e.hasMoreElements();) {
+			String type = colNameType.get(e.nextElement());
+			if(! checkType(type)) {
+				System.out.println("Invalid Type:" + type);
+				return;
+			}
+		}
 		for(Enumeration<String> e = getKeys(colNameType); e.hasMoreElements();) {
 			String colName = e.nextElement();
 			String colType = colNameType.get(colName);
@@ -60,56 +53,126 @@ public class DBApp {
 
 	}
 
-	public void insertIntoTable(String tableName, Hashtable<String, Object> colNameValue) throws DBAppException {
-		Enumeration<String> e = colNameValue.keys();
-		while(e.hasMoreElements())
-		{	
-		Entry entry1 = new Entry(colNameValue, tableName);
-		
+	public static void insertIntoTable(String tableName, Hashtable<String, Object> colNameValue) throws DBAppException, IOException, ClassNotFoundException {
+		for(Enumeration<String> e = colNameValue.keys();e.hasMoreElements();) {
+			String curr = e.nextElement();
+			if(! checkInputType(tableName, curr, colNameValue.get(curr))) {
+				System.out.println("Wrong Input type for " + curr);
+				return;
+			}
 		}
-		
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		fis = new FileInputStream("./Data/" + tableName + ".ser");
+		in = new ObjectInputStream(fis);
+		Table t = (Table) in.readObject();
+		in.close();
+		Entry entry = new Entry(colNameValue, tableName);
+		t.insert(tableName, entry);
 	}
 
 	public void updateTable(String tableName, String key, Hashtable<String,Object> colNameValue) throws DBAppException {
 
 	}
 
-	public void deleteFromTable(String tableName, Hashtable<String,Object> colNameType) throws DBAppException {
+	public void deleteFromTable(String tableName, Hashtable<String,Object> colNameType) throws DBAppException, IOException {
 		FileInputStream fis = null;
-        ObjectInputStream in = null;
-        Table t;
-        try {
-            fis = new FileInputStream(tableName);
-            in = new ObjectInputStream(fis);
-            t = (Table) in.readObject();
-            in.close();
+		ObjectInputStream in = null;
+		Table t;
+		try {
+			fis = new FileInputStream(tableName);
+			in = new ObjectInputStream(fis);
+			t = (Table) in.readObject();
+			in.close();
 
-    		for (int i=0; i<=t.getPages().size(); i++) {
-    			 for (int j=0; j<=t.getPages().get(i).getData().length; j++) {
-    				 for (int k=0; k<t.getPages().get(i).getData()[j].getRow().size(); k++)
-    				  if (t.getPages().get(i).getData()[j].getRow().get(k).equals(colNameType))
-    					  t.getPages().get(i).getData()[j].getRow().remove(k);   }}}
-	catch (Exception ex) {
-            ex.printStackTrace();
-        }
+			for (int i=0; i<=t.getPages().size(); i++) {
+				for (int j=0; j<=t.getPages().get(i).getData().length; j++) {
+					for (int k=0; k<t.getPages().get(i).getData()[j].getRow().size(); k++)
+						if (t.getPages().get(i).getData()[j].getRow().get(k).equals(colNameType))
+							t.getPages().get(i).getData()[j].getRow().remove(k);   }}}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public void selectFromTable(String tableName, String columnName, Object[] objarrValues, String[] strarrOperators) throws DBAppException {
 
 	}
-
-	public static void main(String[] args) throws IOException, DBAppException {
-		init();
-		String strTableName = "Student";
-		Hashtable htblColNameType = new Hashtable( );
-		htblColNameType.put("id", "java.lang.Integer");
-		htblColNameType.put("name", "java.lang.String");
-		htblColNameType.put("gpa", "java.lang.double");
-		createTable( strTableName, "id", htblColNameType );
-	}
+	
 
 	public static Enumeration<String> getKeys(Hashtable<String,String> in){
 		return in.keys();
 	}
 
+	public static String[] getAcceptedTypes() {
+		return acceptedTypes;
+	}
+
+	public static void setAcceptedTypes(String[] acceptedTypes) {
+		DBApp.acceptedTypes = acceptedTypes;
+	}
+
+	public static boolean checkType(String type) {
+		boolean ret = false;
+		for (String acc : acceptedTypes) {
+			if(acc.equals(type)) {
+				ret = true;
+			}
+		}
+		return ret;
+	}
+	
+	public static boolean checkInputType(String tableName, String colName, Object o) throws IOException {
+		boolean ret = false;
+		List<String> list=new ArrayList<>();
+		FileReader fr = new FileReader("./MetaData.csv");
+		BufferedReader br = new BufferedReader(fr);
+		String curr = br.readLine();
+		while(curr != null) {
+			if(curr.contains("" + tableName + "," + colName)) {
+				String type = o.getClass().getName();
+				if(curr.contains(type)) {
+					ret = true;
+				}
+			}
+			curr = br.readLine();
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) throws IOException, DBAppException, ClassNotFoundException {
+		String strTableName = "Student";
+		Hashtable<String, String> htblColNameType = new Hashtable<String, String>( );
+		htblColNameType.put("id", "java.lang.Integer");
+		htblColNameType.put("name", "java.lang.String");
+		htblColNameType.put("gpa", "java.lang.Double");
+		createTable( strTableName, "id", htblColNameType );
+
+		Hashtable<String, Object> htblColNameValue = new Hashtable<String, Object>( );
+		htblColNameValue.put("id", new Integer( 2343432 ));
+		htblColNameValue.put("name", new String("Ahmed Noor" ) );
+		htblColNameValue.put("gpa", new Double(0.95));
+		insertIntoTable( strTableName , htblColNameValue );
+		htblColNameValue.clear( );
+		htblColNameValue.put("id", new Integer( 453455 ));
+		htblColNameValue.put("name", new String("Ahmed Noor" ) );
+		htblColNameValue.put("gpa", new Double( 0.95 ) );
+		insertIntoTable( strTableName , htblColNameValue );
+		htblColNameValue.clear( );
+		htblColNameValue.put("id", new Integer( 5674567 ));
+		htblColNameValue.put("name", new String("Dalia Noor" ) );
+		htblColNameValue.put("gpa", new Double( 1.25 ) );
+		insertIntoTable( strTableName , htblColNameValue );
+		htblColNameValue.clear( );
+		htblColNameValue.put("id", new Integer( 23498 ));
+		htblColNameValue.put("name", new String("John Noor" ) );
+		htblColNameValue.put("gpa", new Double( 1.5 ) );
+		insertIntoTable( strTableName , htblColNameValue );
+		htblColNameValue.clear( );
+		htblColNameValue.put("id", new Integer( 78452 ));
+		htblColNameValue.put("name", new String("Zaky Noor" ) );
+		htblColNameValue.put("gpa", new Double( 0.88 ) );
+		insertIntoTable( strTableName , htblColNameValue );
+	}
+	
 }
--- classes/metaTeam/Table.java
+++ classes/metaTeam/Table.java
@@ -10,6 +10,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
@@ -17,112 +18,176 @@ public class Table implements Serializable{
 	private String key;
 	private ArrayList<Page> pages;
 	private ArrayList<String> order;
+	private String tableName;
+	private int keyIndex;
 
 	public ArrayList<String> getOrder() {
 		return order;
 	}
 
 	public Table (String key, String tableName, Hashtable<String,String> colName) throws IOException {
+		order = new ArrayList<String>();
 		pages = new ArrayList<Page>();
 		this.key = key;
+		this.tableName = tableName;
+		
+		for(Enumeration<String> e = colName.keys(); e.hasMoreElements();) {
+			String curr = e.nextElement();
+			if(curr.equals(key)) {
+				keyIndex = order.size();
+			}
+			order.add(curr);
+		}
+		createPage(tableName);
+		updateSer();
+	}
+
+	public void insert(String tableName, Entry entry) throws IOException, ClassNotFoundException {
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		fis = new FileInputStream("./Data/" + tableName + ".ser");
+		in = new ObjectInputStream(fis);
+		Table t = (Table) in.readObject();
+		ArrayList<Page> pages = t.pages;
+		int pageCount = pages.size() - 1;
+		fis = new FileInputStream("./Data/" + tableName + pageCount + ".ser");
+		in = new ObjectInputStream(fis);
+		Page p = (Page) in.readObject();
+		int entryCount = getEntryCount(p.getData());
+		if(entryCount == 200) {
+			createPage(tableName);
+			pageCount++;
+			fis = new FileInputStream("./Data/" + tableName + (pageCount - 1) + ".ser");
+			in = new ObjectInputStream(fis);
+			p = (Page) in.readObject();
+		}
+		in.close();
+		p.insert(entry, entryCount);
+	}
+	
+	public void createPage(String tableName) throws IOException {
+		String pageName = "" + tableName + this.pages.size();
+		Page p = new Page(pageName,keyIndex);
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(new File("./Data/" + pageName + ".ser"));
+			out = new ObjectOutputStream(fos);
+			out.writeObject(p);
+			out.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		this.pages.add(p);
+		updateSer();
+	}
+
+	public void updateSer() {
 		FileOutputStream fos = null;
 		ObjectOutputStream out = null;
 		try {
 			fos = new FileOutputStream(new File("./Data/" + tableName + ".ser"));
 			out = new ObjectOutputStream(fos);
 			out.writeObject(this);
-
-			out.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-
-
-		for(Enumeration e = colName.keys(); e.hasMoreElements();) {
-			order.add((String) e.nextElement());
-		}
 	}
-
-	public void createPage(String tableName) throws IOException {
-		Page p = new Page();
-		FileOutputStream fos = null;
-		ObjectOutputStream out = null;
-		try {
-			fos = new FileOutputStream(new File("./Data/" + tableName + this.pages.size() + ".ser"));
-			out = new ObjectOutputStream(fos);
-			out.writeObject(p);
-
-			out.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		this.pages.add(p);
-	}
-
+	
 	public ArrayList<Page> getPages() {
 		return this.pages;
-
 	}
+	
 	public void setPages(ArrayList<Page> pages) {
 		this.pages=pages;
 	}
 
+	public int getEntryCount(Entry[] entries) {
+		int count = 0;
+		for (Entry entry : entries) {
+			if(entry != null) {
+				count++;
+			}
+		}
+		return count;
+	}
 }
 
 class Page implements Serializable{
-
+	private String pageName;
 	private Entry[] data;
+	private int keyIndex;
 
-	Page() throws IOException{
+	Page(String pageName, int keyIndex) throws IOException{
 		FileReader fr = new FileReader("./config/DBApp.properties");
 		BufferedReader br = new BufferedReader(fr);
 		String curr = br.readLine();
 		String[] h = curr.split("= ");
 		int curr0 = Integer.parseInt(h[1]);
 		data = new Entry[curr0];
+		this.pageName = pageName;
+		this.keyIndex = keyIndex;
 	}
-	
+
 	public void setData(Entry[] data) {
 		this.data=data;
 	}
-	
+
 	public Entry[] getData() {
 		return this.data;
 	}
-}
-
-class Entry{
-	private ArrayList<Object> row;
-	private ArrayList<String> order;
 	
-	Entry(Hashtable<String,Object> entry,String tablename){
-		for(Enumeration<String> e = entry.keys();e.hasMoreElements();) {
-			FileInputStream fir = null;
-			ObjectInputStream in = null;
-
-			try {
-				fir = new FileInputStream(new File("./Data/" +"Student"+ ".ser"));
-				in = new ObjectInputStream(fir);
-				Table t = (Table)in.readObject();
-				order = t.getOrder();
-				in.close();
-				Enumeration<String> e1 = entry.keys();
-				int index =order.indexOf(e1.nextElement());
-				Object toenter=entry.get(e1.nextElement());
-				row.add(index, toenter);
-
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-
+	public void insert(Entry entry, int entryCount) {
+		data[entryCount] = entry;
+		updatePage();
 	}
 	
+	public void updatePage() {
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(new File("./Data/" + pageName + ".ser"));
+			out = new ObjectOutputStream(fos);
+			out.writeObject(this);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
+
+class Entry implements Serializable{
+	private ArrayList<Object> row;
+	private ArrayList<String> order;
+
+	Entry(Hashtable<String,Object> entry,String tablename){
+
+		row = new ArrayList<Object>();
+		FileInputStream fir = null;
+		ObjectInputStream in = null;
+
+		try {
+			fir = new FileInputStream(new File("./Data/" + tablename + ".ser"));
+			in = new ObjectInputStream(fir);
+			Table t = (Table) in.readObject();
+			order = t.getOrder();
+			in.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		for (String col : order) {
+			Object curr = entry.get(col);
+			row.add(curr);
+		}
+		Date d = new Date();
+		row.add(d);
+		
+	}
+
 	public ArrayList<Object> getRow() {
 		return this.row;
 
 	}
-	
+
 	public void setRow(ArrayList<Object> row) {
 		this.row=row;
 	}
